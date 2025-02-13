efc1edb393f5f23f8022ed329498ea6ea9732018
Basstr98 <s4ernesto1@gmail.com>
2025-02-13 10:46:51 +0100

    Pom, alguna excepcion, controlador comienzo

--- APIRESTful/src/main/java/com/ticarum/apirest/presentacion/Controlador.java
+++ APIRESTful/src/main/java/com/ticarum/apirest/presentacion/Controlador.java
@@ -9,10 +9,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ticarum.apirest.infraestructura.SensorServicio;
-import com.ticarum.rest.application.exceptions.EntidadNoExisteException;
+
 import com.ticarum.apirest.aplicacion.SensorDto;
 import com.ticarum.apirest.aplicacion.SensorHistDto;
 import com.ticarum.apirest.dominio.Sensor;
+import com.ticarum.apirest.excepciones.NoExisteEntidadException;
 import com.ticarum.apirest.infraestructura.HistoricoServicio;
 @RestController
 public class Controlador {
@@ -23,24 +24,25 @@ public class Controlador {
 	@Autowired
 	HistoricoServicio historicoServicio;
 	
+
 	@GetMapping("/sensores")
 	public List<SensorDto> listarSensores() {
 		List<Sensor> sensores = sensorServicio.listar();
-		List<SensorDto> dtos = new LinkedList<SensorDto>();
+		List<SensorDto> sdtos = new LinkedList<SensorDto>();
 		for(Sensor sensor : sensores) {
-			SensorDto dto = sensorServicio.toDto(sensor, SensorDto.class);
-			dtos.add(dto);
+			SensorDto sdto = sensorServicio.toDto(sensor, SensorDto.class);
+			sdtos.add(sdto);
 		}
-		return dtos;
+		return sdtos;
 	}
 	
 	@GetMapping("/sensores/{id}")
 	public SensorHistDto getSensorById(@PathVariable(name="id") String id) {
 		return sensorServicio.getSensor(Long.parseLong(id)).map(sensor -> {
-			SensorHistDto dto = (SensorHistDto) sensorServicio.toDto(sensor, SensorHistDto.class);
-			dto.getHistoricos().forEach(h -> h.setSensor(null));
-			return dto;
-		}).orElseThrow(() -> new EntidadNoExisteException("No existe alumno con ID " + id));
+			SensorHistDto sdto = (SensorHistDto) sensorServicio.toDto(sensor, SensorHistDto.class);
+			sdto.getHistoricos().forEach(h -> h.setSensor(null));
+			return sdto;
+		}).orElseThrow(() -> new NoExisteEntidadException("No existe el sensor con ID " + id));
 	
 	}
 }
